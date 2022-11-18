2c9983492f8c7a5c86e39aeb7d333341c1e40a7f
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-11-18 07:05:46 +0300

    Day3 BlogApp More updates

--- app/src/main/java/com/sosdeveloper/blogapp/Blog.kt
+++ app/src/main/java/com/sosdeveloper/blogapp/Blog.kt
@@ -4,7 +4,6 @@ import android.text.Layout.Alignment
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.ColumnScopeInstance.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
